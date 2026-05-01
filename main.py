c98cd5ef1af52136031c35dca19288164a9f2b1b
faisalbuttdev <faisalbutt.dev@gmail.com>
2026-05-01 14:58:24 +0500

    Model API and Templating

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from fastapi import Fastapi
-app=Fastapi()
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import joblib
+
+models=joblib.load("house-model.pkl")
+templates=Jinja2Templates("templates")
+
+
+app=FastAPI()
+
 
 @app.get("/")
 async def func():
-    return f"housepricing"
+    return f"newproject"
+
+
+@app.get("/model",response_class=HTMLResponse)
+async def model(request:Request):
+    new_data=[[3,2,1500,4000,1,0,0,3]]
+    output=models.predict(new_data).item()#item gives the value only instead of the 2d array
+    return templates.TemplateResponse(request=request,name="index.html",context={"output":output})
